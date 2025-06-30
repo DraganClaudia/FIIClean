4d05dada0a5be3b3fc368c33e23cad70b0de77af
Dragan Claudia <80895901+DraganClaudia@users.noreply.github.com>
2025-06-30 12:27:55 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once 'config/database.php';
+require_once __DIR__ . '/config/database.php';
 $db = new Database();
 $db->connect();
 $db->initTables();
@@ -12,12 +12,12 @@ $resource = $_GET['resource'] ?? 'locations';
 
 switch($resource) {
     case 'locations':
-        require_once 'controllers/LocationController.php';
+        require_once __DIR__ . '/controllers/LocationController.php';
         $controller = new LocationController();
         $controller->handleRequest();
         break;
     case 'orders':
-        require_once 'controllers/OrderController.php';
+        require_once __DIR__ . '/controllers/OrderController.php';
         $controller = new OrderController();
         $controller->handleRequest();
         break;
