c821311cbf47ba59cfa05548aa156330c782cc91
Dragan Claudia <80895901+DraganClaudia@users.noreply.github.com>
2025-07-02 02:39:24 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/config/Database.php';
 $db = new Database();
 $db->connect();
 $db->initTables();
